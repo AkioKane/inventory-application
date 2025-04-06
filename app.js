55307157fb81705c9fe51d9de9836d98a47bee3e
Akio <aakocherga@mail.ru>
2025-04-06 11:11:24 +0700

    Added header

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const path = require("path");
 const app = express();
 
 const indexRouter = require("./routes/indexRouter");
-// const newMessageRouter = require("./routes/newMessageRouter");
 
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
+const assetsPathPublic = path.join(__dirname, "public");
+const assetsPathAssets = path.join(__dirname, "assets");
+const assetsPathFonts = path.join(__dirname, "fonts")
+
+app.use("/public", express.static(assetsPathPublic));
+app.use("/assets", express.static(assetsPathAssets));
+app.use("/fonts", express.static(assetsPathFonts));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// app.use("/new", newMessageRouter);
 app.use("/", indexRouter);
 
 const PORT = 3000;
