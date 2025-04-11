6ae93f8c432188799c84927f656f87ef58eeb785
Akio Kane <aakocherga@mail.ru>
2025-04-12 00:45:14 +0700

    Added items each

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,14 @@
-const indexRouterGet = (req, res) => {
-  res.render("index", {title: "Home"})
+const db = require("../db/quaries");
+
+async function getItems(req, res) {
+  const items = await db.getAllItems();
+  console.log("Items: ", items)
+  return items
+}
+
+async function indexRouterGet(req, res) {
+  const arr = await getItems();
+  res.render("index", {title: "Home", items: arr})
 }
 
 module.exports = {
