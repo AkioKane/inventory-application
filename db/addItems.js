171fea751502266a318a056b013d79c4dc2eb283
Akio Kane <aakocherga@mail.ru>
2025-04-13 00:00:45 +0700

    Added localString() to cost value

--- db/addItems.js
+++ db/addItems.js
@@ -55,10 +55,10 @@ CREATE TABLE IF NOT EXISTS items (
 
 INSERT INTO items (name, gun, cost, category, rarity, quality, imgURL)
 VALUES
-  ('${items[0].name}', '${items[0].gun}', '${items[0].cost}', '${items[0].category}', '${items[0].rarity}', '${items[0].quality}', '${items[0].imgURL}'),
-  ('${items[1].name}', '${items[1].gun}', '${items[1].cost}', '${items[1].category}', '${items[1].rarity}', '${items[1].quality}', '${items[1].imgURL}'),
-  ('${items[2].name}', '${items[2].gun}', '${items[2].cost}', '${items[2].category}', '${items[2].rarity}', '${items[2].quality}', '${items[2].imgURL}'),
-  ('${items[3].name}', '${items[3].gun}', '${items[3].cost}', '${items[3].category}', '${items[3].rarity}', '${items[3].quality}', '${items[3].imgURL}');
+  ('${items[0].name}', '${items[0].gun}', '${items[0].cost.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}', '${items[0].category}', '${items[0].rarity}', '${items[0].quality}', '${items[0].imgURL}'),
+  ('${items[1].name}', '${items[1].gun}', '${items[1].cost.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}', '${items[1].category}', '${items[1].rarity}', '${items[1].quality}', '${items[1].imgURL}'),
+  ('${items[2].name}', '${items[2].gun}', '${items[2].cost.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}', '${items[2].category}', '${items[2].rarity}', '${items[2].quality}', '${items[2].imgURL}'),
+  ('${items[3].name}', '${items[3].gun}', '${items[3].cost.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}', '${items[3].category}', '${items[3].rarity}', '${items[3].quality}', '${items[3].imgURL}');
 `;
 
 async function main() {
