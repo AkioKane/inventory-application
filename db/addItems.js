31515479ca7f867ab50c567eec8dd510c1b2d0c1
Akio Kane <aakocherga@mail.ru>
2025-04-12 23:52:15 +0700

    Added info container for item

--- db/addItems.js
+++ db/addItems.js
@@ -1,26 +1,39 @@
+const { name } = require("ejs");
 const { Client } = require("pg");
 require('dotenv').config();
 
 const items = [
   {
+    name: "Head-Shot",
+    gun: "AK-47",
+    cost: "641",
     category: "Rifle",
     rarity: "Covert",
     quality: "Field-Tested",
     imgURL: "/assets/ak47_headshot.png"
   },
   {
+    name: "Head-Shot",
+    gun: "AK-47",
+    cost: "641",
     category: "Rifle",
     rarity: "Covert",
     quality: "Field-Tested",
     imgURL: "/assets/ak47_headshot.png"
   },
   {
+    name: "Head-Shot",
+    gun: "AK-47",
+    cost: "641",
     category: "Rifle",
     rarity: "Covert",
     quality: "Field-Tested",
     imgURL: "/assets/ak47_headshot.png"
   },
   {
+    name: "Head-Shot",
+    gun: "AK-47",
+    cost: "641",
     category: "Rifle",
     rarity: "Covert",
     quality: "Field-Tested",
@@ -31,24 +44,27 @@ const items = [
 const SQL = `
 CREATE TABLE IF NOT EXISTS items (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT,
+  gun TEXT,
+  cost TEXT,
   category TEXT,
   rarity TEXT,
   quality TEXT,
   imgURL TEXT
 );
 
-INSERT INTO items (category, rarity, quality, imgURL)
+INSERT INTO items (name, gun, cost, category, rarity, quality, imgURL)
 VALUES
-  ('${items[0].category}', '${items[0].rarity}', '${items[0].quality}', '${items[0].imgURL}'),
-  ('${items[1].category}', '${items[1].rarity}', '${items[1].quality}', '${items[1].imgURL}'),
-  ('${items[2].category}', '${items[2].rarity}', '${items[2].quality}', '${items[2].imgURL}'),
-  ('${items[3].category}', '${items[3].rarity}', '${items[3].quality}', '${items[3].imgURL}');
+  ('${items[0].name}', '${items[0].gun}', '${items[0].cost}', '${items[0].category}', '${items[0].rarity}', '${items[0].quality}', '${items[0].imgURL}'),
+  ('${items[1].name}', '${items[1].gun}', '${items[1].cost}', '${items[1].category}', '${items[1].rarity}', '${items[1].quality}', '${items[1].imgURL}'),
+  ('${items[2].name}', '${items[2].gun}', '${items[2].cost}', '${items[2].category}', '${items[2].rarity}', '${items[2].quality}', '${items[2].imgURL}'),
+  ('${items[3].name}', '${items[3].gun}', '${items[3].cost}', '${items[3].category}', '${items[3].rarity}', '${items[3].quality}', '${items[3].imgURL}');
 `;
 
 async function main() {
   console.log("seeding...");
   const client = new Client({
-    connectionString: "process.env.DATABASE_URL",
+    connectionString: process.env.DATABASE_URL,
   });
   await client.connect();
   await client.query(SQL);
